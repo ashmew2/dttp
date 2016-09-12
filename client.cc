647ec9deb94f8981a5a43e1247636f934709976d
Ashish Gupta <ashmew2@gmail.com>
2016-09-13 07:51:14 +0900

    Add client driver

---
+++ client.cc
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUF_SIZE 500
+
+/****
+Purpose of this file is to act as a client
+For example, a web browser and simply contact the tracker and subsequently the seeds.
+Once mature enough, most of this code should go into the Proxy Layer
+****/
+
+int
+main(int argc, char *argv[])
+{
+
+}
